a1b049df5b327e55d36d971a2122d4ed2f027baf
A.Popov <a.popov@kit.team>
2020-12-02 02:05:25 +0500

    Доработка форм регистрации и авторизации

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,55 +1,38 @@
 @extends('main')
 
-@section('main')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
+@section('body')
+    <body class="body-auth text-center vh-100">
+        <main class="form-signin">
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('register') }}">
-                            @csrf
+                <a href="{{ route('index') }}" class="d-block mb-4">
+                    <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 51 51" height="57" width="57" version="1.1">
+                        <g>
+                            <path style="fill:#c00;fill-rule:evenodd" d="m 37,0 14.2087,0 0,50.9653 -14.2087,0 0,-4.2521 8.9707,0 0,-42.4608 -8.9707,0 0,-4.2524 z m -7.1725,18.3309 3.9311,0 0,14.3057 -3.9311,0 0,-1.1938 2.046,0 0,-11.9184 -2.046,0 0,-1.1935 z m 3.8465,-7.7636 8.1974,0 0,29.8333 -8.1974,0 0,-2.4896 4.5349,0 0,-24.8548 -4.5349,0 0,-2.4889 z"></path>
+                            <path style="fill:#c00;fill-rule:evenodd" d="m 14,0 -14.2087,0 0,50.9653 14.2087,0 0,-4.2521 -8.9708,0 0,-42.4608 8.9708,0 0,-4.2524 z m 7.1725,18.3309 -3.9311,0 0,14.3057 3.9311,0 0,-1.1938 -2.046,0 0,-11.9184 2.046,0 0,-1.1935 z m -3.8465,-7.7636 -8.1981,0 0,29.8333 8.1981,0 0,-2.4896 -4.535,0 0,-24.8548 4.535,0 0,-2.4889 z"></path>
+                        </g>
+                    </svg>
+                </a>
 
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                <h1 class="h3 mb-3 fw-normal">Регистрация</h1>
 
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                <label for="email" class="visually-hidden">E-мail адрес</label>
+                <input type="email" id="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="E-мail адрес" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                @error('email')
+                    <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                @enderror
 
-                                    @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
+                <label for="name" class="visually-hidden">Имя</label>
+                <input type="text" id="name" class="form-control @error('name') is-invalid @enderror" name="name" placeholder="Имя" value="{{ old('name') }}" required autocomplete="name">
+                @error('name')
+                    <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                @enderror
 
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                <button class="btn btn-lg btn-primary mt-3" type="submit">Зарегистрироваться</button>
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Register') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                <p class="mt-5 mb-3 text-muted"><a href="{{ route('login') }}">Есть аккаунт?</a></p>
+            </form>
+        </main>
+    </body>
 @endsection
