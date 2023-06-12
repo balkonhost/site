eb627a5da890e6016725b49f12c3159f78eace15
Anton Popov <a.popov@kit.team>
2023-06-12 14:02:45 +0500

    Смена основных цветов

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,15 @@
 @extends('main')
 
 @section('slider')
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <div id="slider" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#slider" data-slide-to="0" class="active"></li>
+            <li data-target="#slider" data-slide-to="1"></li>
         </ol>
         <div class="carousel-inner">
-            <div class="carousel-item active" style="background-color: #333;">
+            <div class="carousel-item active" style="background-color: #00ccff;">
                 <div class="container">
-                    <div class="carousel-caption text-left" style="background-image: url('/img/night.png'); background-position: right center; background-repeat: no-repeat; background-size: contain">
+                    <div class="carousel-caption text-left">
                         <h1 class="carousel-title">Мы гарантируем,<br> что кошмарнее хостинга<br> вы еще не видели.</h1>
                         <p></p>
                     </div>
@@ -24,11 +24,11 @@
                 </div>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#slider" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </a>
