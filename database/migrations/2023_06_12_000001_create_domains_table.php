fc4d996d5d7bf99734fbdee667bbb924c35b197e
Anton Popov <a.popov@kit.team>
2023-06-16 14:43:43 +0500

    Набросок структуры таблиц для доменов пользователей

---
+++ database/migrations/2023_06_12_000001_create_domains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domains', function (Blueprint $table) {
+            $table->id()->comment('Идентификатор');
+            $table->string('name')->unique()->comment('Доменное имя');
+            $table->enum('state', ['active', 'inactive', 'suspended', 'deleted', 'moved'])->default('inactive')->comment('Состояние');
+            $table->date('creation_date');
+            $table->date('expiration_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domains');
+    }
+};
---
+++ database/migrations/2023_06_12_000002_create_provider_domain_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provider_domain', function (Blueprint $table) {
+            $table->foreignId('provider_id')
+                ->constrained('providers')
+                ->cascadeOnDelete()
+                ->comment('Идентификатор поставщика');
+
+            $table->foreignId('domain_id')
+                ->constrained('domains')
+                ->cascadeOnDelete()
+                ->comment('Идентификатор домена');
+
+            $table->unique(['provider_id', 'domain_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provider_domain');
+    }
+};
