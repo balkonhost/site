7dcc46e8ee053269423550d784149a32e6319ea0
Anton Popov <a.popov@kit.team>
2023-06-22 14:59:06 +0500

    Поверка на возможность регистрации домена

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -15,6 +15,29 @@ class DomainController extends Controller
         return view('domain', compact('tlds'));
     }
 
+    public function check(Request $request, Tld $tld, Factory $factory)
+    {
+        $request->flash();
+
+        $view = view('domain')->with('tlds', $tld->active()->get());
+
+        if (($domain = $request->get('domain')) && preg_match('/^[-a-z0-9]+\.[a-z]{2,6}$/', $domain)) {
+            $parts = explode('.', $domain, 2);
+            $tld = $tld->where('tld', end($parts))->with('prices')->first();
+
+            $whois = $factory->createWhois();
+            try {
+                $available = $whois->isDomainAvailable($domain);
+            } catch (Iodev\Whois\Exceptions\ServerMismatchException $exception) {
+                $available = false;
+            }
+
+            $view->with(compact('tld', 'available'));
+        }
+
+        return $view;
+    }
+
     public function whois(Request $request, Factory $factory)
     {
         $view = view('domain.whois');
--- resources/views/domain.blade.php
+++ resources/views/domain.blade.php
@@ -30,35 +30,72 @@
             <li>Мы постоянно забываем уведомить об окончании срока регистрации домена.</li>
             <li>Нет никакой гарантии низких цен на продление на все время пользования доменом.</li>
         </ul>
+    </div>
 
-
-        <h2 class="mt-5">Цены на регистрацию доменов</h2>
-            <p>
-                Доступна регистрация доменов в {{ $tlds->count() }}
-                {{ trans_choice('зоне|зонах|зонах', $tlds->count()) }}.
-            </p>
-
-            <div class="row row-cols-1 row-cols-md-5 g-4 mb-5">
-                @foreach($tlds as $tld)
-                    <div class="col">
-                        <div class="card h-100">
-                            <!--img src="..." class="card-img-top" alt="..."-->
-                            <div class="card-body">
-                                <input type="checkbox" class="form-check-input" name="tld[{{ $tld->tld }}]">
-                                <h5 class="card-title d-inline-block">.{{ $tld->tld }}</h5>
-                                <p class="card-text">
-                                    Регистрация {{ $tld->reg_price }} руб.<br>
-                                    Продление {{ $tld->renew_price }} руб.
-                                </p>
+    <form method="POST" id="check_submit">
+        @csrf
+        <div class="domain" style="background: #f8f8f8; border: 1px solid #dedede;">
+            <div class="container">
+                <div style="padding: 30px 0;">
+                    <div class="d-flex">
+                        <div class="h3 text-nowrap me-5">Проверьте, свободен ли домен</div>
+                        <div class="ml-5 w-100">
+                            <div class="input-group">
+                                <input type="text" name="domain" class="form-control" id="domain" value="{{ old('domain') }}" placeholder="Введите адрес домена" autocomplete="off">
+                                <button class="btn btn-outline-secondary" type="submit">Проверить</button>
                             </div>
-                                <div class="precents">
-                                    <span>-22%</span>
-                                </div>
                         </div>
                     </div>
-                @endforeach
+
+                    @if (old('domain'))
+                        <div id="check_domain">
+                            @isset($tld)
+                                @if ($available)
+                                    @if ($tlds->where('tld', $tld->tld)->count())
+                                        <div>Домен {{ old('domain') }} доступен для регистрации.</div>
+                                    @else
+                                        <div>Домен {{ old('domain') }} доступен для регистрации, но не у нас.</div>
+                                    @endif
+                                @else
+                                    <div>Вас опеределили! Домен {{ old('domain') }} уже зарегистрирован.<br>
+                                        Не верите? Посмотрите сами
+                                        <a href="{{ route('domain.whois', ['domain' => old('domain')]) }}"
+                                           target="_blank" title="Информацию о домене {{ old('domain') }}">информацию о домене</a>.
+                                    </div>
+                                @endif
+                            @else
+                                <div>Домен {{ old('domain') }} нельзя зарегистрировать.</div>
+                            @endisset
+                        </div>
+                    @endif
+                </div>
             </div>
+        </div>
+    </form>
 
+    <div class="container my-5">
+        <h2 class="mt-5">Цены на регистрацию доменов</h2>
+        <p>
+            Доступна регистрация доменов в {{ $tlds->count() }}
+            {{ trans_choice('зоне|зонах|зонах', $tlds->count()) }}.
+        </p>
 
+        <div class="row row-cols-1 row-cols-md-5 g-4 mb-5">
+            @foreach($tlds as $tld)
+                <div class="col">
+                    <div class="card h-100">
+                        <!--img src="..." class="card-img-top" alt="..."-->
+                        <div class="card-body">
+                            <h5 class="card-title d-inline-block">.{{ $tld->tld }}</h5>
+                            <p class="card-text">
+                                Регистрация {{ $tld->reg_price }} руб.<br>
+                                Продление {{ $tld->renew_price }} руб.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
+
 @endsection
