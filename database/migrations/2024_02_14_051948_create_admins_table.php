4cc64510e275a97e40da89094ef24f39acf5f7d8
Anton Popov <a.popov@kit.team>
2024-02-15 15:48:04 +0500

    FIX

--- database/migrations/2024_02_14_051948_create_admins_table.php
+++ database/migrations/2024_02_14_051948_create_admins_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('members', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('position');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('members');
+        Schema::dropIfExists('admins');
     }
 };
