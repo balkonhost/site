e30b33c7640ed1f3e2fc71fe9654058158bb3538
Anton Popov <a.popov@kit.team>
2024-02-15 14:25:10 +0500

    Временное отключение всех разделов.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DomainController;
 use App\Http\Controllers\CartController;
-use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PostController;
 use App\Services\RegRu\DomainService;
 use App\Models\Admin;
 
@@ -24,12 +24,16 @@ Route::get('/', function () {
 })->name('index');
 
 // Разговоры на балконе
-Route::group(['prefix' => 'blog'], function () {
-    Route::get('/', 'BlogController@index')->name('blog');
-    Route::get('/{post}', 'BlogController@show')->name('blog.show');
-});
+/*Route::resource('blog', PostController::class)->names([
+    'index' => 'blog',
+    'create' => 'blog.create',
+    'edit' => 'blog.edit',
+    'update' => 'blog.update',
+    'show' => 'blog.show',
+    'destroy' => 'blog.destroy'
+]);*/
 
-Route::group(['prefix' => 'domain'], function () {
+/*Route::group(['prefix' => 'domain'], function () {
     Route::get('', [DomainController::class, 'index'])
         ->name('domain');
     Route::post('', [DomainController::class, 'check'])
@@ -37,58 +41,68 @@ Route::group(['prefix' => 'domain'], function () {
     Route::get('whois', [DomainController::class, 'whois'])
         ->name('domain.whois')
         ->middleware('throttle:1,0.02');
-});
+});*/
 
-Route::get('/hosting', function () {
+/*Route::get('/hosting', function () {
     return view('hosting');
-})->name('hosting');
+})->name('hosting');*/
 
-Route::get('/server', function () {
+/*Route::get('/server', function () {
     return view('server');
-})->name('server');
+})->name('server');*/
 
-Route::get('/ssl', function () {
+/*Route::get('/ssl', function () {
     return view('ssl');
-})->name('ssl');
+})->name('ssl');*/
 
-Route::get('/cms', function () {
+/*Route::get('/cms', function () {
     return view('cms');
-})->name('cms');
+})->name('cms');*/
 
-Route::get('/support', function () {
+/*Route::get('/support', function () {
     return view('support');
-})->name('support');
+})->name('support');*/
 
-Route::get('/info', function () {
+/*Route::get('/info', function () {
     return view('info');
-})->name('info');
+})->name('info');*/
 
-Route::get('/info/team', function (Admin $admin) {
+/*Route::get('/info/team', function (Admin $admin) {
     return view('team',  ['admins' => $admin->all()]);
-})->name('team');
+})->name('team');*/
 
-Route::get('/info/data-center', function () {
+/*Route::get('/info/data-center', function () {
     return view('data-center');
-})->name('data-center');
+})->name('data-center');*/
 
-Route::group(['prefix' => 'cart'], function () {
+/*Route::group(['prefix' => 'cart'], function () {
     Route::get('', [CartController::class, 'list'])->name('cart');
     Route::post('add', [CartController::class, 'add'])->name('cart.add');
     Route::get('remove', [CartController::class, 'remove'])->name('cart.remove');
     Route::get('clear', [CartController::class, 'clear'])->name('cart.clear');
-});
+});*/
 
-Route::post('/logout', function () {
+/*Route::post('/logout', function () {
     auth()->guard()->logout();
     return redirect()->back();
-})->name('logout');
+})->name('logout');*/
 
 //Route::get('/test', function (DomainService $service) {
 //    dd($service->getPrices());
 //    echo 'test';
 //});
 
-Route::get('/test', function () {
+/*Route::get('/test', function () {
     $user = \App\Models\User::find(0);
     dd($user->domains);
-});
+});*/
+
+//Route::resource('', Controller::class)->names([
+//    'index' => 'thoughts',
+//    'create' => 'thoughts.create',
+//    'store' => 'thoughts.store',
+//    'edit' => 'thoughts.edit',
+//    'update' => 'thoughts.update',
+//    'show' => 'thoughts.show',
+//    'destroy' => 'thoughts.destroy'
+//]);
