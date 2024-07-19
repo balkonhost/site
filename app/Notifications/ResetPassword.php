81c48a52c78b58875331d65deca8e039bafe0eb8
Anton Popov <a.popov@kit.team>
2024-07-19 11:10:52 +0500

    Письмо с запросом смены пароля

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
+
+class ResetPassword extends Notifications\ResetPassword
+{
+    protected function buildMailMessage($url)
+    {
+        return (new MailMessage)
+            ->subject(Lang::get('Запрос на смену шифра'))
+            ->line(Lang::get('Ты получил эту электронную маляву, потому что был получен запрос на смену шифра от твоего сейфа у нас на Балконе.'))
+            ->line(Lang::get('По доброте нашей сердечной, сменить шифр ты можешь в течение :count минут.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->action(Lang::get('Сменить шифр'), $url)
+            ->line(Lang::get('Если это не ты отправлял запрос, а какой-то чепушило, никаких дальнейших действий не требуется.'));
+    }
+}
