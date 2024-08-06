19d05bc176d0c5d335b66a65574c0fe3aa482d0a
Anton Popov <a.popov@kit.team>
2024-08-06 15:49:15 +0500

    Изменение макета

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,55 +3,57 @@
 @section('title', 'Личный кабинет / Балкон.Хост')
 @section('description', 'Крайне неудобный и дурацкий личный кабинет.')
 
-@section('breadcrumb')
+@section('header')
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('index') }}">Главная</a></li>
             <li class="breadcrumb-item active" aria-current="page">Личный кабинет</li>
         </ol>
     </nav>
+
+    <h1>Личный кабинет</h1>
 @endsection
 
-@section('content')
-    <h1>Привет {{ auth()->user()->name }}!</h1>
-    <p>У которого на счете {{ auth()->user()->balance }} ₽. <a href="{{ route('home.balance') }}" title="История операций">Глянем</a> как так вышло?</p>
+@section('menu')
+    <div class="list-group">
+
+        @if (Route::has('home.balance'))
+            @if (Route::is('home.balance*'))
+                <a href="{{ route('home.balance') }}" class="list-group-item list-group-item-action active" aria-current="true">Баланс</a>
+            @else
+                <a href="{{ route('home.balance') }}" class="list-group-item list-group-item-action">Баланс</a>
+            @endif
+        @endif
+
+        @if (Route::has('home.domain'))
+            @if (Route::is('home.domain*'))
+                <a href="{{ route('home.domain') }}" class="list-group-item list-group-item-action active" aria-current="true">Домены</a>
+            @else
+                <a href="{{ route('home.domain') }}" class="list-group-item list-group-item-action">Домены</a>
+            @endif
+        @endif
+
+        @if (Route::has('home.hosting'))
+            @if (Route::is('home.hosting*'))
+                <a href="{{ route('home.hosting') }}" class="list-group-item list-group-item-action active" aria-current="true">Хостинг</a>
+            @else
+                <a href="{{ route('home.hosting') }}" class="list-group-item list-group-item-action">Хостинг</a>
+            @endif
+        @endif
+
+    </div>
 @endsection
 
 @section('main')
     <div class="container my-5">
 
-        @yield('breadcrumb')
+        @yield('header')
 
         <div class="row">
             <div class="col-md-12 col-lg-3">
                 <div class="side mb-5">
-                    <div class="list-group">
 
-                        @if (Route::has('home.balance'))
-                            @if (Route::is('home.balance*'))
-                                <a href="{{ route('home.balance') }}" class="list-group-item list-group-item-action active" aria-current="true">Баланс</a>
-                            @else
-                                <a href="{{ route('home.balance') }}" class="list-group-item list-group-item-action">Баланс</a>
-                            @endif
-                        @endif
+                    @yield('menu')
 
-                        @if (Route::has('home.domain'))
-                            @if (Route::is('home.domain*'))
-                                <a href="{{ route('home.domain') }}" class="list-group-item list-group-item-action active" aria-current="true">Домены</a>
-                            @else
-                                <a href="{{ route('home.domain') }}" class="list-group-item list-group-item-action">Домены</a>
-                            @endif
-                        @endif
-
-                        @if (Route::has('home.hosting'))
-                            @if (Route::is('home.hosting*'))
-                                <a href="{{ route('home.hosting') }}" class="list-group-item list-group-item-action active" aria-current="true">Хостинг</a>
-                            @else
-                                <a href="{{ route('home.hosting') }}" class="list-group-item list-group-item-action">Хостинг</a>
-                            @endif
-                        @endif
-
-                    </div>
                 </div>
             </div>
             <div class="col-md-12 col-lg-9">
