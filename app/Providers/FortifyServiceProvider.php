87c51b3495ef02a16622e7d8b27d23f46717e47d
Anton Popov <a.popov@kit.team>
2024-08-13 13:07:56 +0500

    Текущие изменения

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -73,6 +73,7 @@ class FortifyServiceProvider extends ServiceProvider
                 if ($user->isDirty()) {
                     $user->email_verified_at = Carbon::now();
                     if ($user->save() && isset($temp)) {
+                        $user->wallet;
                         $temp->delete();
                     }
                 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/hata';
 
     /**
      * The controller namespace for the application.
@@ -47,7 +47,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
-            Route::prefix('home')
+            Route::prefix('hata')
                 ->middleware(['web','auth'])
                 ->namespace($this->namespace)
                 ->group(base_path('routes/home.php'));
